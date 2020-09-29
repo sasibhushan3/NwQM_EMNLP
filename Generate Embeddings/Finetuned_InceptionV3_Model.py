3d95e1f84198a33efaf6807bafe7baeb33714d88
Sasi Bhushan Seelaboyina <34206015+sasibhushan3@users.noreply.github.com>
2020-09-29 20:27:47 +0530

    Update Finetuned_InceptionV3_Model.py

--- Generate Embeddings/Finetuned_InceptionV3_Model.py
+++ Generate Embeddings/Finetuned_InceptionV3_Model.py
@@ -91,7 +91,7 @@ def create_model():
 
 # Command line arguments
 parser = argparse.ArgumentParser(description='Read Arguments for Finetuned InceptionV3 model')
-parser.add_argument('--dataset_path', type=str, nargs='?', default='wikipages_SplToken1.csv',
+parser.add_argument('--dataset_path', type=str, nargs='?', default='wikipages.csv',
                                         help='dataset path')
 parser.add_argument('--images_path', type=str, nargs='?', default='wiki_images/',
                                         help='path of the folder containing images of the pages')
